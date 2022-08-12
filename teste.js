9637b90dbc00bb244653529997db0bbfd98346b8
Bilica <101262548+BilicaGabriella@users.noreply.github.com>
2022-08-12 09:54:26 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,16 @@
+// console.log("Olá node")
+const prompt = require("prompt-sync")();
+
+// const nun1 = Number(prompt("Digite o número 1: "));
+// // console.log(nun1);
+// const nun2 = Number(prompt("Digite o número 2: "));
+
+// const somar = nun1 + nun2;
+
+// //aspas contraria ``
+// console.log(`A soma dos númmeros é: ${somar}`);
+
+function tabuada(){
+    for(var count=1; count<=10 ; count++)
+     document.write("3 x "+count+" = " + (3*count) + "<br />");
+  }
